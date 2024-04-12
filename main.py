2f6ddf40e3e9ab2fbe7d09dcf09d9e3382d60b2b
Nathan Romasanta <naytanromasanta@gmail.com>
2024-04-12 09:20:36 -0600

    added the sort function

--- main.py
+++ main.py
@@ -4,8 +4,47 @@ import os
 import shutil
 
 
+directory = ""
+
 def sortFiles( _path ):
-    pass
+    for filename in os.listdir(_path):
+                f = os.path.join(_path, filename)
+                new_path = ""
+                # checking if it is a file
+                if os.path.isfile(f):
+                    try: 
+                        if f.endswith('.mp3'):
+                            new_path = directory + '/Music/'
+                            shutil.move(f, new_path)
+
+                        elif f.endswith(".ai") or f.endswith(".psd") or f.endswith(".prproj") or f.endswith(".psb"):
+                            new_path = directory + '/SourceFiles/' 
+                            shutil.move(f, new_path)
+
+                        elif f.endswith(".mp4"):
+                            new_path = directory + '/Videos/' 
+                            shutil.move(f, new_path)
+
+                        elif f.endswith(".txt"):
+                            new_path = directory + '/Texts/' 
+                            shutil.move(f, new_path)
+
+                        elif f.endswith(".docx") or f.endswith(".pdf") or f.endswith(".ppt") or f.endswith(".xlsx"):
+                            new_path = directory + '/Documents/' 
+                            shutil.move(f, new_path)
+
+                        elif f.endswith(".png") or f.endswith(".jpeg") or f.endswith(".webp") or f.endswith(".jpg"):
+                            new_path = directory + '/Images/' 
+                            shutil.move(f, new_path)
+
+                        else:
+                            new_path = directory + '/Others/' 
+                            shutil.move(f, new_path)
+
+
+                    
+                    except Exception as e:
+                        print(f"{e}")
 
 
 def main():
@@ -25,14 +64,14 @@ def main():
             os.mkdir(os.path.join(directory, "Images"))
             os.mkdir(os.path.join(directory, "Others"))
     
-            sortFiles()
+            sortFiles(directory)
 
         except FileExistsError:
             print("Folders Already Exists!")
             choice = input("Are you sure you want to continue? (y/n): ")
 
             if choice.lower() == "y":
-                sortFiles()
+                sortFiles(directory)
             else:
                 exit
 
